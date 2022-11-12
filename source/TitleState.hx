325475fe4732f91af8c894678638065d8d47679e
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-12 16:16:01 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -38,7 +38,7 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		#if sys
+		#if windows 
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
 		#end
